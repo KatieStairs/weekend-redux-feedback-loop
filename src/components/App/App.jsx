30e27986b66b8b4a3bf7cd89067d37fe59c31f3c
KatieStairs <111089579+KatieStairs@users.noreply.github.com>
2023-01-21 10:46:53 -0600

    Initial commit

---
+++ src/components/App/App.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import axios from 'axios';
+import './App.css';
+
+function App() {
+
+  return (
+    <div className='App'>
+      <header className='App-header'>
+        <h1 className='App-title'>Feedback!</h1>
+        <h4>Don't forget it!</h4>
+      </header>
+    </div>
+  );
+}
+
+export default App;
